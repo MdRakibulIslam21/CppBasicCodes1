85484adc78330f20f51c4e9a9d993f895b2ce2ce
Md Rakibul Islam <mahbubulrakib@gmail.com>
2025-08-20 19:24:38 +0600

    Add files via upload

---
+++ LuckyNum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a=n/10;
+    int b=n%10;
+    if(a%b==0 || b%a==0){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+}
