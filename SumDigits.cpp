64c2347956e2288b6e5d6182c251c88a72e41c54
Md Rakibul Islam <mahbubulrakib@gmail.com>
2025-08-25 22:33:06 +0600

    Add files via upload

---
+++ SumDigits.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    string a;
+    cin>>a;
+    int sum=0;
+    for(int i=0;i<n;i++){
+        sum=sum+a[i]-'0';
+    }
+    cout<<sum<<endl;
+}
