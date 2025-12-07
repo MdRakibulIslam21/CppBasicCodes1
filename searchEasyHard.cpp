f9c87907bb2cd39bac74eeb1090f141512b57b92
Md Rakibul Islam <mahbubulrakib@gmail.com>
2025-12-07 12:17:00 +0600

    Add files via upload

---
+++ searchEasyHard.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int input;
+    bool hard = false;
+
+    for (int i = 0; i < n; i++) {
+        cin >>input;
+        if (input == 1) {
+            hard = true;
+        }
+    }
+
+    if (hard) {
+        cout << "HARD";
+    } else {
+        cout << "EASY";
+    }
+
+    return 0;
+}
+
